e5279331270941a66bd67e4543c0e30f86889454
MJoshu <marvicjoshuasalazar0602@gmail.com>
2024-03-04 12:14:37 +0800

    updated Home.php, Database.php, init.php, Model.php

--- mvc/app/Controllers/Home.php
+++ mvc/app/Controllers/Home.php
@@ -4,6 +4,11 @@ class Home extends Controller
 {
     public function index()
     {
+        $model = new Model();
+        $arr['firstname'] = 'Marvic Joshua';
+        $arr2['lastname'] = 'Santos';
+        $data =$model->where($arr, $arr2);
+        show($data);
         $this->view('home');
     }
 }
