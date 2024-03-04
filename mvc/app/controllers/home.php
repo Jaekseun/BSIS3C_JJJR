7857122f99c41678139dffca25613104dce84d92
Jaekseun <jacksonpasana17@gmail.com>
2024-03-04 12:17:28 +0800

    did some update

--- mvc/app/controllers/home.php
+++ mvc/app/controllers/home.php
@@ -4,6 +4,11 @@ class Home extends Controller
 {
     public function index()
     {
+        $model = new Model();
+        $arr['lastname'] = 'Pasana';
+        $arr2['firstname'] = 'Ryan Carlo';
+        $data = $model->where($arr , $arr2);
+        show($data);
         $this->view('home');
     }
 }
