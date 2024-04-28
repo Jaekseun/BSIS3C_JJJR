38a97c18948225b0a25081d55c550a321981eeaa
Jaekseun <jacksonpasana17@gmail.com>
2024-04-28 16:33:28 +0800

    added lists-of-tasks

--- lists-of-tasks/app/core/config.php
+++ lists-of-tasks/app/core/config.php
@@ -1,10 +1,9 @@
 <?php
 
 define('ROOT', 'http://localhost/BSIS3C_WEBA_24/mvc-test/public');
-define('PATH', 'C:/xampp/htdocs/BSIS3C_WEBA_24/mvc-test/app/views/');
-
+define('PATH','C:/xampp/htdocs/BSIS3C_WEBA_24/mvc-test/app/views/');
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'mvc_bsis3c');
+define('DB_NAME', 'tasks_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
